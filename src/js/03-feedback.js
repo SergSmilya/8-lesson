b1abe0c550704c5b80eaf706c0301b10d95aedb7
SergSmilya <sergsmilichenko@gmail.com>
2023-01-16 18:16:09 +0200

    task 2.5

---
+++ src/js/03-feedback.js
@@ -0,0 +1,32 @@
+import throttle from 'lodash.throttle';
+const buttonEl = document.querySelector('.feedback-form');
+const formEl = document.querySelector('.feedback-form');
+
+buttonEl.addEventListener('submit', onButtonClick);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+const dataForm = {};
+saveData();
+
+function onButtonClick(e) {
+  e.preventDefault();
+
+  console.log(localStorage.getItem('feedback-form-state'));
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(e) {
+  dataForm[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+function saveData() {
+  const dataFromStorage = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (dataFromStorage) {
+    formEl.elements.email.value = dataFromStorage.email ?? '';
+    formEl.elements.message.value = dataFromStorage.message ?? '';
+  }
+}
